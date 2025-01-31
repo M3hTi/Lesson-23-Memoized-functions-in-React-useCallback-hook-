d87f11b3e84b4cecb05a10383a8893f763238914
M3hTi <mehdinasir502@yahoo.com>
2025-01-31 13:35:02 +0330

    feat: add search input component with label and button in App

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,31 @@ import './App.css'
 function App() {
   return (
     <>
-      
+      <h1>REST Countries</h1>
+      <InputWithLabelAndButton id="search" typ="text" >
+        Search Country:
+      </InputWithLabelAndButton>
+    </>
+  )
+}
+
+
+function InputWithLabelAndButton({id,type,children}){
+  const inputRef = React.useRef(null)
+
+  React.useEffect(() => {
+    if(inputRef.current){
+      inputRef.current.focus()
+    }
+  },[inputRef])
+
+
+  return(
+    <>
+      <label htmlFor={id}>{children}</label>
+      &nbsp;
+      <input type={type} id={id} ref={inputRef} />
+      <button>Search</button>
     </>
   )
 }
